a83d4aecf287509884bac57d6219650b227fe945
Marc Raphael <marcraphael979@gmail.com>
2021-10-09 23:49:30 +0000

    Add a function to toggle the burger button lines

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const nav = document.querySelector(".elements");
 const button = document.querySelector(".rows");
 const lines = document.querySelectorAll(".span");
+
+
+function toggler() {
+  nav.classList.toggle("sidebar");
+  lines[0].classList.toggle("line-0");
+  lines[1].classList.toggle("line-1");
+  lines[2].classList.toggle("line-2");
+}
